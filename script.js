8d670e8250674934d69eeea68507139e8323364d
Jason Ozulumba <cjozulumba@gmail.com>
2020-09-05 23:49:08 -0400

    Got buttons to read

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 var timer = document.getElementById("timer");
 var userChoice = document.getElementById("buttonGroup");
+var answers = userChoice.children;
 
 var secondsLeft = 60;
 
 function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timer.textContent = "Timer: " + secondsLeft;
-  
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-      }
-  
-    }, 1000);
-  }
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timer.textContent = "Timer: " + secondsLeft;
 
- var check = userChoice.addEventListener("click", function() {
-     console.log("hello world");
-     if (userChoice.children.getElementById("btn1") == btn1) {
-         console.log("button 1 works");
-     }
- })
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
 
- document.addEventListener("click", check);
+var buttonCheck = userChoice.addEventListener("click", function (event) {
+    var buttons = event.target;
+    if (buttons.matches("button")) {
+        var whichChoice = buttons.getAttribute("id");
+
+        if(whichChoice === "btn1") {
+            console.log("Button 1")
+        }
+        if(whichChoice === "btn2") {
+            console.log("Button 2")
+        }
+        if(whichChoice === "btn3") {
+            console.log("Button 3")
+        }
+        if(whichChoice === "btn4") {
+            console.log("Button 4")
+        }
+    }
+});
+
+document.addEventListener("click", buttonCheck);
